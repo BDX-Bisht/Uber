8cf6ac374023649e5b1fe7258d95c4f61fb841b2
dipanshu-vivobook <dipanshu.vivobook@gmail.com>
2026-03-02 22:25:23 +0530

    feat: implement user model and database connection, add bcrypt and jsonwebtoken dependencies

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,10 @@ dotenv.config();
 
 const cors = require("cors");
 const express = require("express");
+const connectToDb = require("./db/db");
 const app = express();
+
+connectToDb();
 
 app.use(cors());
 
